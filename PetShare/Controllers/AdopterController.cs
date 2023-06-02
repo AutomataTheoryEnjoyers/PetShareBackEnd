6d16e143dd1019d9f26651504457d070f998ffe3
Karol Kącki <karol.kacki2@gmail.com>
2023-06-02 08:29:14 +0200

    adopters pagination

--- PetShare/Controllers/AdopterController.cs
+++ PetShare/Controllers/AdopterController.cs
@@ -4,6 +4,7 @@ using PetShare.Models;
 using PetShare.Models.Adopters;
 using PetShare.Services;
 using PetShare.Services.Interfaces.Adopters;
+using PetShare.Services.Interfaces.Pagination;
 
 namespace PetShare.Controllers;
 
@@ -13,12 +14,14 @@ public sealed class AdopterController : ControllerBase
 {
     private readonly IAdopterCommand _command;
     private readonly IAdopterQuery _query;
+    private readonly IPaginationService _paginator;
     private readonly TokenValidator _validator;
 
-    public AdopterController(IAdopterQuery query, IAdopterCommand command, TokenValidator validator)
+    public AdopterController(IAdopterQuery query, IAdopterCommand command, IPaginationService paginator, TokenValidator validator)
     {
         _query = query;
         _command = command;
+        _paginator = paginator;
         _validator = validator;
     }
 
@@ -27,15 +30,22 @@ public sealed class AdopterController : ControllerBase
     /// </summary>
     [HttpGet]
     [Authorize(Roles = Roles.Admin)]
-    [ProducesResponseType(typeof(IReadOnlyList<AdopterResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(PaginatedAdoptersResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    public async Task<ActionResult<IReadOnlyList<AdopterResponse>>> GetAll()
+    public async Task<ActionResult<PaginatedAdoptersResponse>> GetAll([FromQuery] PaginationQueryRequest paginationQuery)
     {
         if (!await _validator.ValidateClaims(User))
             return Unauthorized();
 
-        return (await _query.GetAllAsync()).Select(a => a.ToResponse()).ToList();
+        var adopters = (await _query.GetAllAsync()).Select(a => a.ToResponse()).ToList();
+
+        var paginatedAdopters = _paginator.GetPage<AdopterResponse>(adopters, paginationQuery);
+        if(paginatedAdopters == null)
+            return BadRequest("Wrong pagination parameters");
+
+        return PaginatedAdoptersResponse.FromPaginatedResult(paginatedAdopters.Value);
     }
 
     /// <summary>
---
+++ PetShare/Models/Adopters/PaginatedAdoptersResponse.cs
@@ -0,0 +1,21 @@
+﻿using PetShare.Services.Interfaces.Pagination;
+
+namespace PetShare.Models.Adopters
+{
+    public sealed class PaginatedAdoptersResponse
+    {
+        public required IReadOnlyList<AdopterResponse> Adopters { get; set; }
+        public required int PageNumber { get; set; }
+        public required int Count { get; set; }
+
+        public static PaginatedAdoptersResponse FromPaginatedResult(PaginatedResult<AdopterResponse> result)
+        {
+            return new PaginatedAdoptersResponse
+            {
+                Adopters = result.items.ToList(),
+                Count = result.totalCount,
+                PageNumber = result.pageNr,
+            };
+        }
+    }
+}

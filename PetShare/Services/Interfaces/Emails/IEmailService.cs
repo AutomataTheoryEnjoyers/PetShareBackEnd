5dda816ff8087f99ca2e1e2d66012ec2efdae008
Meeso1 <kubaknyspel@gmail.com>
2023-06-02 14:49:30 +0200

    Remove 'public' from email interface

--- PetShare/Services/Interfaces/Emails/IEmailService.cs
+++ PetShare/Services/Interfaces/Emails/IEmailService.cs
@@ -2,8 +2,8 @@
 
 public interface IEmailService
 {
-    public Task SendEmailAsync(string recipientEmail, string recipientUsername, object dynamicTemplateData,
+    Task SendEmailAsync(string recipientEmail, string recipientUsername, object dynamicTemplateData,
         string templateId);
 
-    public Task SendStatusUpdateEmail(string recipientEmail, string recipientUsername, string newStatus);
+    Task SendStatusUpdateEmail(string recipientEmail, string recipientUsername, string newStatus);
 }

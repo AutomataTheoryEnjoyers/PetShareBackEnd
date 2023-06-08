b26e94af6f9879aa0c82f33b330731131bd38b2c
Meeso1 <kubaknyspel@gmail.com>
2023-06-08 11:10:05 +0200

    Controller annotations

--- PetShare/Controllers/ReportController.cs
+++ PetShare/Controllers/ReportController.cs
@@ -29,6 +29,7 @@ public class ReportController : ControllerBase
     [HttpGet]
     [Authorize(Roles = Roles.Admin)]
     [ProducesResponseType(typeof(ReportPageResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
     public async Task<ActionResult<ReportPageResponse>> GetAll([FromQuery] PaginationQueryRequest request)
@@ -51,8 +52,8 @@ public class ReportController : ControllerBase
     /// </summary>
     [HttpPost]
     [AllowAnonymous]
-    [ProducesResponseType(StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ReportResponse), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(NotFoundResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult> Post(ReportRequest request)
     {
         var report = Report.FromRequest(request);
@@ -68,10 +69,10 @@ public class ReportController : ControllerBase
     [HttpPut]
     [Route("{id:guid}")]
     [Authorize(Roles = Roles.Admin)]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ReportResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(NotFoundResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ReportResponse>> Put(Guid id, ReportUpdateRequest request)
     {
         var state = Enum.Parse<ReportState>(request.State, true);

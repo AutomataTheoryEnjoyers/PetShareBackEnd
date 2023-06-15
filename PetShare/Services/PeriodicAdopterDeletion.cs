dcb882c12e62b1fe25792177721f6ff02b14220b
Meeso1 <39009805+Meeso1@users.noreply.github.com>
2023-06-15 22:28:38 +0200

    Removing deleted adopters (#59)

---
+++ PetShare/Services/PeriodicAdopterDeletion.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Options;
+using PetShare.Configuration;
+using PetShare.Services.Interfaces.Adopters;
+
+namespace PetShare.Services;
+
+public sealed class PeriodicAdopterDeletion : BackgroundService
+{
+    private readonly IOptions<AdopterDeletionConfiguration> _configuration;
+    private readonly IServiceScopeFactory _scopeFactory;
+
+    public PeriodicAdopterDeletion(IServiceScopeFactory factory,
+        IOptions<AdopterDeletionConfiguration> configuration)
+    {
+        _scopeFactory = factory;
+        _configuration = configuration;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken token)
+    {
+        using var timer = new PeriodicTimer(_configuration.Value.DeletionPeriod);
+        while (!token.IsCancellationRequested)
+            try
+            {
+                using var scope = _scopeFactory.CreateScope();
+                var command = scope.ServiceProvider.GetRequiredService<IAdopterCommand>();
+                var limit = DateTime.Now - TimeSpan.FromDays(_configuration.Value.RetentionDays);
+                await command.RemoveDeletedAsync(limit, token);
+                await timer.WaitForNextTickAsync(token);
+            }
+            catch (OperationCanceledException)
+            {
+                return;
+            }
+    }
+}
